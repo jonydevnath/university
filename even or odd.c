a914cb46e136d2a60cb7edf3978481dad19b5c36
Jony Devnath <76802139+jonydevnath@users.noreply.github.com>
2023-05-24 15:37:18 +0600

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+// upload in the github
+// Home work total 15 complete in beecrowed problem set
+int main(void)
+{
+    printf("Enter Your Number: ");
+    int num;
+    scanf("%d", &num);
+
+    if(num%2 == 0)
+    {
+        printf("Number is even");
+    }
+    else
+    {
+        printf("Number is odd");
+    }
+
+}
